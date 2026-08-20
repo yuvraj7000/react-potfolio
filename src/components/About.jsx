f0ac0210ef208c1e8d7b9406e8948bac1c04900f
Yuvraj Yadav <120801567+yuvraj7000@users.noreply.github.com>
2026-08-21 04:28:23 +0530

    Update About Me section with new details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const AboutSection = () => {
         <div className="dd"><p className={`text-3xl font-bold mb-4 animee`}>
       About Me
     </p></div>
-          <p className="text-lg mb-6 para">"I’m a Full-Stack Web Developer with expertise in the MERN stack, Django, and PostgreSQL, currently pursuing a B.Tech in Artificial Intelligence and Data Science at MITS Gwalior. I build scalable web applications and intuitive mobile apps using React Native. With a solid foundation in Data Structures and Algorithms (C++), I’m also skilled in Linux, and my interests extend to Machine Learning and Data Science. I'm passionate about solving real-world problems through technology and constantly pushing my boundaries as a developer."</p>
+          <p className="text-lg mb-6 para">"B.Tech graduate in Artificial Intelligence and Data Science with experience in full-stack development, data engineering, and machine learning. Built scalable applications using React, Node.js, and Spring Boot, along with data pipelines and machine learning models for real-world use cases. Solved 500+ Data Structures and Algorithms (DSA) problems, achieved a 1600+ CodeChef rating, and actively exploring Generative AI and Large Language Model (LLM) systems."</p>
           <button className="bg-yellow-400 text-gray-900 py-2 px-6 rounded-full font-bold hover:bg-yellow-300 transition duration-300" ><a href='#projects'>Projects</a></button>
           {/* <button className="bg-yellow-400 text-gray-900 mx-2 py-2 px-6 rounded-full font-bold hover:bg-yellow-300 transition duration-300"><a href='https://drive.google.com/file/d/1LJZx5lf345CmrHPXUrgHn51C54x3Yslw/view?usp=sharing'>Resume</a></button> */}
         </div>
